74efb8464eed5069569b444254e04ab5dc4cf852
km4891 <kmorarity@gmail.com>
2021-11-20 18:22:07 -0800

    added insert

--- src/HTBT.java
+++ src/HTBT.java
@@ -14,5 +14,33 @@ public class HTBT {
         return size == 0;
     }
 
+    public int getSize () {
+
+        return size;
+    }
+
+    public void insert (int value) {
+
+        size++;
+        int pos = myhash(value);        
+        Node root = table[position];
+        root = insert(root, value);
+        table[position] = root;
+    }
+
+    private Node insert(Node node, int data)
+    {
+        if (node == null)
+            node = new Node(data);
+        else
+        {
+            if (data <= node.data)
+                node.left = insert(node.left, data);
+            else
+                node.right = insert(node.right, data);
+        }
+        return node;
+    }
+
     
 }
